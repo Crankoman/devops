dbbd65fc0a7dfebc214bedf6ead75d671d296f44
Ruslan M <crankoman@gmail.com>
2024-03-10 21:49:27 +0300

    fix

--- diploma/terraform/monitoring.tf
+++ diploma/terraform/monitoring.tf
@@ -1,18 +1,19 @@
 # разворачиваем kube-prometheus 
-resource "null_resource" "monitoring" {
+resource "null_resource" "grafana_deployment" {
   provisioner "local-exec" {
     command = "cd ../kube-prometheus/ && kubectl apply --server-side -f manifests/setup && kubectl wait --for condition=Established --all CustomResourceDefinition --namespace=monitoring && kubectl apply -f manifests/"
   }
   depends_on = [
-    null_resource.kubeconfig_cp
+    null_resource.ansible_provisioner
   ]
 }
+
 # разворачиваем kube-prometheus сервис
-resource "null_resource" "monitoring_service" {
+resource "null_resource" "grafana_service" {
   provisioner "local-exec" {
     command = "kubectl apply -f ../k8s/service-grafana.yaml"
   }
   depends_on = [
-    null_resource.monitoring
+    null_resource.grafana_deployment
   ]
 }
